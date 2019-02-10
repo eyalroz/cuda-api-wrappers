613d8e3bf4cdb1f82cae45a1f358333ab8812cb6
Eyal Rozenberg <eyalroz@technion.ac.il>
2019-02-10 11:47:50 +0200

    Fixes #86
    * Corrected some hard-coded device properties for Pascal, Volta and Turing
    * Added some hard-coded device properties for Volta and Turing

--- src/cuda/api/device_properties.cpp
+++ src/cuda/api/device_properties.cpp
@@ -4,6 +4,17 @@
 #include <unordered_map>
 #include <utility>
 
+// The values hard-coded in this file are based on the information from the following sources:
+//
+// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html
+// https://docs.nvidia.com/cuda/pascal-tuning-guide/index.html
+// https://docs.nvidia.com/cuda/volta-tuning-guide/index.html
+// https://docs.nvidia.com/cuda/turing-tuning-guide/index.html
+// https://en.wikipedia.org/wiki/CUDA
+//
+// See specifically the sections on shared memory capacity in the Tuning and Programming
+// guides -- it's a somewhat confusing issue.
+
 namespace cuda {
 namespace device {
 
@@ -11,12 +22,14 @@ const char* compute_architecture_t::name(unsigned architecture_number)
 {
 	static std::unordered_map<unsigned, std::string> arch_names =
 	{
-		{ 1, "Tesla"   },
-		{ 2, "Fermi"   },
-		{ 3, "Kepler"  },
-		{ 5, "Maxwell" },
-		{ 6, "Pascal"  },
-		{ 7, "Pascal"  },
+		{ 1, "Tesla"          },
+		{ 2, "Fermi"          },
+		{ 3, "Kepler"         },
+		{ 5, "Maxwell"        },
+		{ 6, "Pascal"         },
+		{ 7, "Volta/Turing"   },
+			// Unfortunately, nVIDIA broke with the custom of having the numeric prefix
+			// designate the architecture name, with Turing (Compute Capability 7.5 _only_).
 	};
 	return arch_names.at(architecture_number).c_str();
 		// Will throw for invalid architecture numbers!
@@ -33,11 +46,20 @@ shared_memory_size_t compute_architecture_t::max_shared_memory_per_block() const
 		{ 1,  16 * KiB },
 		{ 2,  48 * KiB },
 		{ 3,  48 * KiB },
-		{ 5,  64 * KiB },
-		{ 6,  64 * KiB },
+		{ 5,  48 * KiB },
+		{ 6,  48 * KiB },
 		{ 7,  96 * KiB },
-			// this is a speculative figure based on:
-			// https://devblogs.nvidia.com/parallelforall/inside-volta/
+			// this is the Volta figure, Turing is different. Also, values above
+			// 48 require a call such as:
+			//
+			// cudaFuncSetAttribute(
+			//     my_kernel,
+			//     cudaFuncAttributePreferredSharedMemoryCarveout,
+			//     cudaSharedmemCarveoutMaxShared
+			// );
+			//
+			// for details, see the CUDA C Programming Guide at:
+			// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html
 	};
 	return data.at(major);
 }
@@ -50,7 +72,7 @@ unsigned compute_architecture_t::max_resident_warps_per_processor() const {
 		{ 3, 64 },
 		{ 5, 64 },
 		{ 6, 64 },
-		{ 7, 64 },
+		{ 7, 64 }, // this is the Volta figure, Turing is different
 	};
 	return data.at(major);
 }
@@ -62,8 +84,8 @@ unsigned compute_architecture_t::max_warp_schedulings_per_processor_cycle() cons
 		{ 2, 2 },
 		{ 3, 4 },
 		{ 5, 4 },
-		{ 6, 2 },
-		{ 7, 2 }, // speculation
+		{ 6, 4 },
+		{ 7, 4 },
 	};
 	return data.at(major);
 }
@@ -76,7 +98,7 @@ unsigned compute_architecture_t::max_in_flight_threads_per_processor() const {
 		{ 3, 192 },
 		{ 5, 128 },
 		{ 6, 128 },
-		{ 7, 128 }, // speculation
+		{ 7, 128 }, // this is the Volta figure, Turing is different
 	};
 	return data.at(major);
 }
@@ -87,8 +109,9 @@ shared_memory_size_t compute_capability_t::max_shared_memory_per_block() const
 	static std::unordered_map<unsigned, unsigned> data =
 	{
 		{ 37, 112 * KiB },
-		{ 52,  96 * KiB },
-		{ 61,  96 * KiB },
+		{ 75,  64 * KiB },
+			// This is the Turing, rather than Volta, figure; but see
+			// the note regarding how to actually enable this
 	};
 	auto cc = as_combined_number();
 	auto it = data.find(cc);
@@ -102,6 +125,7 @@ unsigned compute_capability_t::max_resident_warps_per_processor() const {
 		{ 11, 24 },
 		{ 12, 32 },
 		{ 13, 32 },
+		{ 75, 32 },
 	};
 	auto cc = as_combined_number();
 	auto it = data.find(cc);
@@ -126,6 +150,7 @@ unsigned compute_capability_t::max_in_flight_threads_per_processor() const {
 	{
 		{ 21,  48 },
 		{ 60,  64 },
+		{ 75,  64 },
 	};
 	auto cc = as_combined_number();
 	auto it = data.find(cc);
