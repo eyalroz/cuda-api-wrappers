e8c6b8e79a6ea037ee7bf5db27ebc5e44dd62399
Jonathan von Schroeder <jvs@uni-bremen.de>
2019-03-12 13:58:35 +0000

    always use cmake based compiling for capability detection

--- cmake/Modules/HandleCUDAComputeCapability.cmake
+++ cmake/Modules/HandleCUDAComputeCapability.cmake
@@ -8,36 +8,47 @@
 # those without the CUDA_ prefix
 
 if (NOT CUDA_TARGET_COMPUTE_CAPABILITY)
-	if(WIN32)
-		find_package(CUDA)
-		if (CUDA_FOUND)
-			set(CUDAFILE ${CMAKE_CURRENT_SOURCE_DIR}/examples/check_cuda.cu)
-			try_run(run_result compile_result ${CMAKE_CURRENT_BINARY_DIR} ${CUDAFILE}
-				RUN_OUTPUT_VARIABLE CUDA_TARGET_COMPUTE_CAPABILITY_
-				COMPILE_OUTPUT_VARIABLE compile_output)
-			if(NOT "${compile_result}")
-				message(SEND_ERROR "Detecting CUDA_TARGET_COMPUTE_CAPABILITY failed: Compilation failure")
-				message(SEND_ERROR "${compile_output}")
-			elseif(NOT ("${run_result}" EQUAL "0"))
-				message(SEND_ERROR "Detecting CUDA_TARGET_COMPUTE_CAPABILITY failed: Run failure")
-				message(SEND_ERROR "${CUDA_TARGET_COMPUTE_CAPABILITY_}")
-			endif()
-			separate_arguments(CUDA_TARGET_COMPUTE_CAPABILITY_ UNIX_COMMAND "${CUDA_TARGET_COMPUTE_CAPABILITY_}")
-			list(GET CUDA_TARGET_COMPUTE_CAPABILITY_ 1 FORMATTED_COMPUTE_CAPABILITY)
-			list(GET CUDA_TARGET_COMPUTE_CAPABILITY_ 0 CUDA_TARGET_COMPUTE_CAPABILITY_)
+	if (CMAKE_CUDA_COMPILER_LOADED)
+		set(CUDAFILE ${CMAKE_CURRENT_BINARY_DIR}/check_cuda.cu)
+		file(WRITE ${CUDAFILE}
+"#include <stdio.h>\n\
+#include <cuda_runtime_api.h>\n\
+int main()\n\
+{\n\
+	cudaDeviceProp prop;\n\
+	cudaError_t status;\n\
+	int device_count;\n\
+	status = cudaGetDeviceCount(&device_count);\n\
+	int device_index = 0;\n\
+	if (status != cudaSuccess) {\n\
+		fprintf(stderr,\"cudaGetDeviceCount() failed: %s\\n\", cudaGetErrorString(status));\n\
+		return -1;\n\
+	}\n\
+	if (device_index >= device_count) {\n\
+		fprintf(stderr, \"Specified device index %d exceeds the maximum (the device count on this system is %d)\\n\", device_index, device_count);\n\
+		return -1;\n\
+	}\n\
+	status = cudaGetDeviceProperties(&prop, device_index);\n\
+	if (status != cudaSuccess) {\n\
+		fprintf(stderr,\"cudaGetDeviceProperties() for device ${device_index} failed: %s\\n\", cudaGetErrorString(status));\n\
+		return -1;\n\
+	}\n\
+	printf(\"%d%d %d.%d\", prop.major, prop.minor, prop.major, prop.minor);\n\
+	return 0;\n\
+}")
+		try_run(run_result compile_result ${CMAKE_CURRENT_BINARY_DIR} ${CUDAFILE}
+			RUN_OUTPUT_VARIABLE CUDA_TARGET_COMPUTE_CAPABILITY_
+			COMPILE_OUTPUT_VARIABLE compile_output)
+		if(NOT "${compile_result}")
+			message(SEND_ERROR "Detecting CUDA_TARGET_COMPUTE_CAPABILITY failed: Compilation failure")
+			message(SEND_ERROR "${compile_output}")
+		elseif(NOT ("${run_result}" EQUAL "0"))
+			message(SEND_ERROR "Detecting CUDA_TARGET_COMPUTE_CAPABILITY failed: Run failure")
+			message(SEND_ERROR "${CUDA_TARGET_COMPUTE_CAPABILITY_}")
 		endif()
-	else()
-		if("$ENV{CUDA_SM}" STREQUAL "")
-			set(ENV{CUDA_INCLUDE_DIRS} "${CUDA_INCLUDE_DIRS}")
-			set(ENV{CUDA_CUDART_LIBRARY} "${CUDA_CUDART_LIBRARY}")
-			set(ENV{CMAKE_CXX_COMPILER} "${CMAKE_CXX_COMPILER}")
-			execute_process(COMMAND bash -c "${CMAKE_CURRENT_SOURCE_DIR}/scripts/get_cuda_sm.sh"  OUTPUT_VARIABLE CUDA_TARGET_COMPUTE_CAPABILITY_)
-		else()
-			set(CUDA_TARGET_COMPUTE_CAPABILITY_ $ENV{CUDA_SM})
-		endif()
-
-		execute_process(COMMAND bash -c "echo -n $(echo ${CUDA_TARGET_COMPUTE_CAPABILITY})" OUTPUT_VARIABLE CUDA_TARGET_COMPUTE_CAPABILITY)
-		execute_process(COMMAND bash -c "echo ${CUDA_TARGET_COMPUTE_CAPABILITY} | sed 's/^\\([0-9]\\)\\([0-9]\\)/\\1.\\2/;' | xargs echo -n" OUTPUT_VARIABLE FORMATTED_COMPUTE_CAPABILITY)
+		separate_arguments(CUDA_TARGET_COMPUTE_CAPABILITY_ UNIX_COMMAND "${CUDA_TARGET_COMPUTE_CAPABILITY_}")
+		list(GET CUDA_TARGET_COMPUTE_CAPABILITY_ 1 FORMATTED_COMPUTE_CAPABILITY)
+		list(GET CUDA_TARGET_COMPUTE_CAPABILITY_ 0 CUDA_TARGET_COMPUTE_CAPABILITY_)
 	endif()
 endif()
 
