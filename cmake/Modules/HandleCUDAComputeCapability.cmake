26bdfea4d89224b34a098379a479f0c152b52a93
Jonathan von Schroeder <jvs@uni-bremen.de>
2019-03-15 14:59:14 +0000

    Made changes requested in the pull request:
    * improved variable and file naming
    * add nvcc flags for all detected architectures
    * better handling of the value-formatting

--- cmake/Modules/HandleCUDAComputeCapability.cmake
+++ cmake/Modules/HandleCUDAComputeCapability.cmake
@@ -9,9 +9,13 @@
 
 if (NOT CUDA_TARGET_COMPUTE_CAPABILITY)
 	if (CMAKE_CUDA_COMPILER_LOADED)
-		set(CUDAFILE ${CMAKE_CURRENT_BINARY_DIR}/check_cuda.cu)
+		set(CUDAFILE ${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/query_gpu_compute_capability.cu)
 		file(WRITE ${CUDAFILE}
-"#include <stdio.h>\n\
+"#include <string>\n\
+#include <iostream>\n\
+#include <vector>\n\
+#include <algorithm>\n\
+#include <sstream>\n\
 #include <cuda_runtime_api.h>\n\
 int main()\n\
 {\n\
@@ -19,45 +23,54 @@ int main()\n\
 	cudaError_t status;\n\
 	int device_count;\n\
 	status = cudaGetDeviceCount(&device_count);\n\
-	int device_index = 0;\n\
+	std::vector<std::string> sm_values;\n\
 	if (status != cudaSuccess) {\n\
 		fprintf(stderr,\"cudaGetDeviceCount() failed: %s\\n\", cudaGetErrorString(status));\n\
 		return -1;\n\
 	}\n\
-	if (device_index >= device_count) {\n\
-		fprintf(stderr, \"Specified device index %d exceeds the maximum (the device count on this system is %d)\\n\", device_index, device_count);\n\
-		return -1;\n\
+	for(int device_index = 0; device_index < device_count; device_index++){\n\
+		status = cudaGetDeviceProperties(&prop, device_index);\n\
+		if (status != cudaSuccess) {\n\
+			fprintf(stderr,\"cudaGetDeviceProperties() for device ${device_index} failed: %s\\n\", cudaGetErrorString(status));\n\
+			return -1;\n\
+		}\n\
+		std::ostringstream ss;\n\
+		ss << prop.major << \".\" << prop.minor;\n\
+		sm_values.push_back(ss.str());\n\
 	}\n\
-	status = cudaGetDeviceProperties(&prop, device_index);\n\
-	if (status != cudaSuccess) {\n\
-		fprintf(stderr,\"cudaGetDeviceProperties() for device ${device_index} failed: %s\\n\", cudaGetErrorString(status));\n\
-		return -1;\n\
+	std::vector<std::string>::iterator end = std::unique(sm_values.begin(), sm_values.end()); ;\n\
+	sm_values.resize(std::distance(sm_values.begin(), end));\n\
+	for(std::vector<std::string>::iterator it = sm_values.begin(); it != sm_values.end(); it++) {\n\
+		std::cout << *it;\n\
+		if((it+1) != sm_values.end()) std::cout << \";\";\n\
 	}\n\
-	printf(\"%d%d %d.%d\", prop.major, prop.minor, prop.major, prop.minor);\n\
 	return 0;\n\
 }")
-		try_run(run_result compile_result ${CMAKE_CURRENT_BINARY_DIR} ${CUDAFILE}
+		try_run(RUN_RESULT COMPILE_RESULT ${CMAKE_CURRENT_BINARY_DIR} ${CUDAFILE}
 			RUN_OUTPUT_VARIABLE CUDA_TARGET_COMPUTE_CAPABILITY_
 			COMPILE_OUTPUT_VARIABLE compile_output)
-		if(NOT "${compile_result}")
+		if(NOT "${COMPILE_RESULT}")
 			message(SEND_ERROR "Detecting CUDA_TARGET_COMPUTE_CAPABILITY failed: Compilation failure")
 			message(SEND_ERROR "${compile_output}")
-		elseif(NOT ("${run_result}" EQUAL "0"))
+		elseif(NOT ("${RUN_RESULT}" EQUAL "0"))
 			message(SEND_ERROR "Detecting CUDA_TARGET_COMPUTE_CAPABILITY failed: Run failure")
 			message(SEND_ERROR "${CUDA_TARGET_COMPUTE_CAPABILITY_}")
 		endif()
-		separate_arguments(CUDA_TARGET_COMPUTE_CAPABILITY_ UNIX_COMMAND "${CUDA_TARGET_COMPUTE_CAPABILITY_}")
-		list(GET CUDA_TARGET_COMPUTE_CAPABILITY_ 1 FORMATTED_COMPUTE_CAPABILITY)
-		list(GET CUDA_TARGET_COMPUTE_CAPABILITY_ 0 CUDA_TARGET_COMPUTE_CAPABILITY_)
 	endif()
 endif()
 
-set(CUDA_TARGET_COMPUTE_CAPABILITY "${CUDA_TARGET_COMPUTE_CAPABILITY_}" CACHE STRING "CUDA compute capability of the (first) \
-	CUDA device on the system, in XY format (like the X.Y format but no dot); see table of features and capabilities by \
+set(CUDA_TARGET_COMPUTE_CAPABILITY "${CUDA_TARGET_COMPUTE_CAPABILITY_}" CACHE STRING "List of CUDA compute capabilities of the targeted \
+	CUDA devices in X.Y format - see table of features and capabilities by \
 	capability X.Y value at https://en.wikipedia.org/wiki/CUDA#Version_features_and_specifications")
 
-message(STATUS "CUDA device-side code will assume compute capability ${FORMATTED_COMPUTE_CAPABILITY}")
+message(STATUS "CUDA device-side code will generated for the compute capabilitie(s) ${CUDA_TARGET_COMPUTE_CAPABILITY}")
 
-set(CUDA_GENCODE "arch=compute_${CUDA_TARGET_COMPUTE_CAPABILITY},code=compute_${CUDA_TARGET_COMPUTE_CAPABILITY}")
-set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} -gencode ${CUDA_GENCODE} )
+set(FORMATTED_COMPUTE_CAPABILITY "")
+foreach(COMPUTE_CAPABILITY ${CUDA_TARGET_COMPUTE_CAPABILITY})
+	string(REPLACE "." "" COMPUTE_CAPABILITY "${COMPUTE_CAPABILITY}")
+	string(APPEND FORMATTED_COMPUTE_CAPABILITY " -gencode arch=compute_${COMPUTE_CAPABILITY},code=compute_${COMPUTE_CAPABILITY}")
+endforeach(COMPUTE_CAPABILITY)
 
+message(STATUS "Using extra NVCC flags:${FORMATTED_COMPUTE_CAPABILITY}")
+
+set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} ${FORMATTED_COMPUTE_CAPABILITY})
