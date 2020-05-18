c615e79a397cfa412c2203da8e47b968e43e9259
Eyal Rozenberg <eyalroz@technion.ac.il>
2020-05-18 18:18:23 +0300

    Fixes #182, regards #183, #176.
    * The code in `device_properties.cpp` has been moved into headers.
    * `device_properties.cpp` is removed, so that other than for profiling - `cuda-api-wrappers` is now a header-only library.
    * The implementations have been "dumbed down" to mostly fit into C++11-`constexpr` functions - except for the throwing of exceptions on error. Thus, `constexpr` functions with the relevant functionality are within `cuda::device::detail` and in a `detail/` subdirectory file (no need to manually include it); but the methods of `compute_architecutre_t` and `compute_capability_t` are throwing as before, wrapping the `constexpr` function calls.
    * Made a few more methods `noexcept`.

---
+++ src/cuda/api/detail/device_properties.hpp
@@ -0,0 +1,296 @@
+/**
+ * @file detail/device_properties.hpp
+ *
+ * @brief Implementation of methods and helper functions for device-property-related classes.
+ *
+ */
+#pragma once
+#ifndef CUDA_API_WRAPPERS_DETAIL_DEVICE_PROPERTIES_HPP_
+#define CUDA_API_WRAPPERS_DETAIL_DEVICE_PROPERTIES_HPP_
+
+#include <cuda/api/device_properties.hpp>
+
+namespace cuda {
+namespace device {
+
+// compute_architecture_t-related functions
+
+inline constexpr bool compute_architecture_t::is_valid() const noexcept
+{
+	return (major > 0) and (major < 9999); // Picked this up from somewhere in the CUDA code
+}
+
+inline constexpr bool operator ==(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
+{
+	return lhs.major == rhs.major;
+}
+inline constexpr bool operator !=(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
+{
+	return lhs.major != rhs.major;
+}
+inline constexpr bool operator <(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
+{
+	return lhs.major < rhs.major;
+}
+inline constexpr bool operator <=(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
+{
+	return lhs.major < rhs.major;
+}
+inline constexpr bool operator >(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
+{
+	return lhs.major > rhs.major;
+}
+inline constexpr bool operator >=(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
+{
+	return lhs.major > rhs.major;
+}
+
+namespace detail {
+
+constexpr const int invalid_architecture_return { 0 };
+enum : memory::shared::size_t { KiB = 1024 };
+
+template <typename T>
+inline T ensure_arch_property_validity(T v, const compute_architecture_t& arch)
+{
+	if (v == detail::invalid_architecture_return) {
+		throw std::invalid_argument("No architecture numbered " + std::to_string(arch.major));
+	}
+	return v;
+}
+
+template <>
+inline const char* ensure_arch_property_validity<const char*>(const char* v, const compute_architecture_t& arch)
+{
+	if (v == nullptr) {
+		throw std::invalid_argument("No architecture numbered " + std::to_string(arch.major));
+	}
+	return v;
+}
+
+inline constexpr const char* architecture_name(const compute_architecture_t& arch)
+{
+	return
+		(arch.major == 1) ? "Tesla" :
+		(arch.major == 2) ? "Fermi" :
+		(arch.major == 3) ? "Kepler" :
+			// Note: No architecture number 4!
+		(arch.major == 5) ? "Maxwell" :
+		(arch.major == 6) ? "Pascal" :
+		(arch.major == 7) ? "Volta/Turing" :
+			// Unfortunately, nVIDIA broke with the custom of having the numeric prefix
+			// designate the architecture name, with Turing (Compute Capability 7.5 _only_).
+		(arch.major == 8) ? "Ampere" :
+		nullptr;
+}
+
+inline constexpr memory::shared::size_t max_shared_memory_per_block(const compute_architecture_t& arch)
+{
+	return
+		(arch.major == 1) ?  16 * KiB :
+		(arch.major == 2) ?  48 * KiB :
+		(arch.major == 3) ?  48 * KiB :
+		// Note: No architecture number 4!
+		(arch.major == 5) ?  48 * KiB :
+		(arch.major == 6) ?  48 * KiB :
+		(arch.major == 7) ?  96 * KiB :
+			// this is the Volta figure, Turing is different. Also, values above 48 require a call such as:
+			//
+			// cudaFuncSetAttribute(
+			//     my_kernel,
+			//     cudaFuncAttributePreferredSharedMemoryCarveout,
+			//     cudaSharedmemCarveoutMaxShared
+			// );
+			//
+			// for details, see the CUDA C Programming Guide.
+		invalid_architecture_return;
+}
+
+inline unsigned max_resident_warps_per_processor(const compute_architecture_t& arch)
+{
+	return
+		(arch.major == 1) ?  24 :
+		(arch.major == 2) ?  48 :
+		(arch.major == 3) ?  64 :
+		// Note: No architecture number 4!
+		(arch.major == 5) ?  64 :
+		(arch.major == 6) ?  64 :
+		(arch.major == 7) ?  64 : // this is the Volta figure, Turing is different
+		invalid_architecture_return;
+}
+
+inline unsigned max_warp_schedulings_per_processor_cycle(const compute_architecture_t& arch)
+{
+	return
+		(arch.major == 1) ?  1 :
+		(arch.major == 2) ?  2 :
+		(arch.major == 3) ?  4 :
+		// Note: No architecture number 4!
+		(arch.major == 5) ?  4 :
+		(arch.major == 6) ?  4 :
+		(arch.major == 7) ?  4 :
+		invalid_architecture_return;
+}
+
+inline constexpr unsigned max_in_flight_threads_per_processor(const compute_architecture_t& arch)
+{
+	return
+		(arch.major == 1) ?   8 :
+		(arch.major == 2) ?  32 :
+		(arch.major == 3) ? 192 :
+		// Note: No architecture number 4!
+		(arch.major == 5) ? 128 :
+		(arch.major == 6) ? 128 :
+		(arch.major == 7) ? 128 : // this is the Volta figure, Turing is different
+		invalid_architecture_return;
+}
+
+} // namespace detail
+
+inline const char* compute_architecture_t::name() const {
+	return detail::ensure_arch_property_validity(detail::architecture_name(*this), *this);
+}
+
+inline unsigned compute_architecture_t::max_in_flight_threads_per_processor() const
+{
+	return detail::ensure_arch_property_validity(detail::max_in_flight_threads_per_processor(*this), *this);
+}
+
+inline unsigned compute_architecture_t::max_shared_memory_per_block() const
+{
+	return detail::ensure_arch_property_validity(detail::max_shared_memory_per_block(*this), *this);
+}
+
+inline unsigned compute_architecture_t::max_resident_warps_per_processor() const
+{
+	return detail::ensure_arch_property_validity(detail::max_resident_warps_per_processor(*this), *this);
+}
+
+inline unsigned compute_architecture_t::max_warp_schedulings_per_processor_cycle() const
+{
+	return detail::ensure_arch_property_validity(detail::max_warp_schedulings_per_processor_cycle(*this), *this);
+}
+
+// compute_capability_t-related
+
+inline constexpr bool operator ==(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
+{
+	return lhs.major() == rhs.major() and lhs.minor_ == rhs.minor_;
+}
+inline constexpr bool operator !=(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
+{
+	return lhs.major() != rhs.major() or lhs.minor_ != rhs.minor_;
+}
+inline constexpr bool operator <(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
+{
+	return lhs.major() < rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ < rhs.minor_);
+}
+inline constexpr bool operator <=(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
+{
+	return lhs.major() < rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ <= rhs.minor_);
+}
+inline constexpr bool operator >(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
+{
+	return lhs.major() > rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ > rhs.minor_);
+}
+inline constexpr bool operator >=(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
+{
+	return lhs.major() > rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ >= rhs.minor_);
+}
+
+inline constexpr bool compute_capability_t::is_valid() const noexcept
+{
+	return architecture.is_valid() and (minor_ > 0) and (minor_ < 9999);
+		// Picked this up from the CUDA code somwhere
+}
+
+inline constexpr compute_capability_t compute_capability_t::from_combined_number(unsigned combined) noexcept
+{
+	return  { combined / 10, combined % 10 };
+}
+
+inline constexpr unsigned compute_capability_t::as_combined_number() const noexcept { return major() * 10 + minor_; }
+
+
+inline constexpr compute_capability_t make_compute_capability(unsigned combined) noexcept
+{
+	return compute_capability_t::from_combined_number(combined);
+}
+
+inline constexpr compute_capability_t make_compute_capability(unsigned major, unsigned minor) noexcept
+{
+	return { major, minor };
+}
+
+namespace detail {
+
+inline constexpr unsigned max_in_flight_threads_per_processor(const compute_capability_t& cc)
+{
+	return
+		cc.as_combined_number() == 21 ? 48 :
+		cc.as_combined_number() == 60 ? 64 :
+		cc.as_combined_number() == 75 ? 64 :
+		max_in_flight_threads_per_processor(cc.architecture);
+}
+
+inline constexpr unsigned max_warp_schedulings_per_processor_cycle(const compute_capability_t& cc)
+{
+	return
+		cc.as_combined_number() == 61 ? 4 :
+		cc.as_combined_number() == 62 ? 4 :
+		max_warp_schedulings_per_processor_cycle(cc.architecture);
+}
+
+inline constexpr unsigned max_shared_memory_per_block(const compute_capability_t& cc)
+{
+	return
+		cc.as_combined_number() == 37 ? 112 * KiB :
+		cc.as_combined_number() == 75 ?  64 * KiB :
+		max_shared_memory_per_block(cc.architecture);
+}
+
+
+inline constexpr unsigned max_resident_warps_per_processor(const compute_capability_t& cc)
+{
+	return
+		cc.as_combined_number() == 11 ? 24 * KiB :
+		cc.as_combined_number() == 12 ? 32 * KiB :
+		cc.as_combined_number() == 13 ? 32 * KiB :
+		cc.as_combined_number() == 75 ? 32 * KiB :
+		max_resident_warps_per_processor(cc.architecture);
+}
+
+} // namespace detail
+
+inline unsigned compute_capability_t::max_in_flight_threads_per_processor() const
+{
+	return detail::ensure_arch_property_validity(detail::max_in_flight_threads_per_processor(*this), architecture);
+}
+
+inline unsigned compute_capability_t::max_warp_schedulings_per_processor_cycle() const
+{
+	return detail::ensure_arch_property_validity(detail::max_warp_schedulings_per_processor_cycle(*this), architecture);
+}
+
+inline unsigned compute_capability_t::max_shared_memory_per_block() const
+{
+	return detail::ensure_arch_property_validity(detail::max_shared_memory_per_block(*this), architecture);
+}
+
+inline unsigned compute_capability_t::max_resident_warps_per_processor() const
+{
+	return detail::ensure_arch_property_validity(detail::max_resident_warps_per_processor(*this), architecture);
+}
+
+// properties_t-related
+
+inline bool properties_t::usable_for_compute() const noexcept
+{
+	return computeMode != cudaComputeModeProhibited;
+}
+
+
+} // namespace device
+} // namespace cuda
+
+#endif // CUDA_API_WRAPPERS_DETAIL_DEVICE_PROPERTIES_HPP_
--- src/cuda/api/device_properties.hpp
+++ src/cuda/api/device_properties.hpp
@@ -15,6 +15,8 @@
 
 #include <cuda_runtime_api.h>
 
+#include <stdexcept>
+
 
 // The following un-definitions avoid warnings about
 // the use of `major` and `minor` in certain versions
@@ -42,54 +44,25 @@ struct compute_architecture_t {
 	/**
 	 * A @ref compute_capability_t has a "major" and a "minor" number,
 	 * with "major" indicating the architecture; so this struct only
-	 * has a "major" numner
+	 * has a "major" number
 	 */
 	unsigned major;
 
-	static const char* name(unsigned major_compute_capability_version);
+	const char* name() const;
 	unsigned max_warp_schedulings_per_processor_cycle() const;
 	unsigned max_resident_warps_per_processor() const;
 	unsigned max_in_flight_threads_per_processor() const;
+
 	/**
 	 * @note On some architectures, the shared memory / L1 balance is configurable,
 	 * so you might not get the maxima here without making this configuration
 	 * setting
 	 */
 	memory::shared::size_t max_shared_memory_per_block() const;
-	const char* name() const { return name(major); }
 
-	bool is_valid() const noexcept
-	{
-		return (major > 0) and (major < 9999); // Picked this up from the CUDA code somwhere
-	}
+	constexpr bool is_valid() const noexcept;
 
 };
-
-inline bool operator ==(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
-{
-	return lhs.major == rhs.major;
-}
-inline bool operator !=(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
-{
-	return lhs.major != rhs.major;
-}
-inline bool operator <(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
-{
-	return lhs.major < rhs.major;
-}
-inline bool operator <=(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
-{
-	return lhs.major < rhs.major;
-}
-inline bool operator >(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
-{
-	return lhs.major > rhs.major;
-}
-inline bool operator >=(const compute_architecture_t& lhs, const compute_architecture_t& rhs) noexcept
-{
-	return lhs.major > rhs.major;
-}
-
 
 // TODO: Consider making this a non-POD struct,
 // with a proper ctor checking validity, an operator converting to pair etc;
@@ -108,7 +81,13 @@ struct compute_capability_t {
 	compute_architecture_t architecture;
 	unsigned minor_;
 
-	unsigned as_combined_number() const noexcept { return major() * 10 + minor_; }
+	constexpr static compute_capability_t from_combined_number(unsigned combined) noexcept;
+
+	constexpr unsigned major() const { return architecture.major; }
+	unsigned constexpr minor() const { return minor_; }
+
+	constexpr unsigned as_combined_number() const noexcept;
+	constexpr bool is_valid() const noexcept;
 	unsigned max_warp_schedulings_per_processor_cycle() const;
 	unsigned max_resident_warps_per_processor() const;
 	unsigned max_in_flight_threads_per_processor() const;
@@ -119,59 +98,11 @@ struct compute_capability_t {
 	 */
 	memory::shared::size_t max_shared_memory_per_block() const;
 
-	unsigned major() const { return architecture.major; }
 
-	// We don't really need this method, but it allows for the same access pattern as for the
-	// major number, i.e. major() and minor(). Alternatively, we could have
-	// used a proxy
-	unsigned minor() const { return minor_; }
-
-	bool is_valid() const
-	{
-		return (major() > 0) and (major() < 9999) and (minor_ > 0) and (minor_ < 9999);
-			// Picked this up from the CUDA code somwhere
-	}
-
-	static compute_capability_t from_combined_number(unsigned combined)
-	{
-		return  { combined / 10, combined % 10 };
-	}
 };
 
-inline bool operator ==(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
-{
-	return lhs.major() == rhs.major() and lhs.minor_ == rhs.minor_;
-}
-inline bool operator !=(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
-{
-	return lhs.major() != rhs.major() or lhs.minor_ != rhs.minor_;
-}
-inline bool operator <(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
-{
-	return lhs.major() < rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ < rhs.minor_);
-}
-inline bool operator <=(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
-{
-	return lhs.major() < rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ <= rhs.minor_);
-}
-inline bool operator >(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
-{
-	return lhs.major() > rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ > rhs.minor_);
-}
-inline bool operator >=(const compute_capability_t& lhs, const compute_capability_t& rhs) noexcept
-{
-	return lhs.major() > rhs.major() or (lhs.major() == rhs.major() and lhs.minor_ >= rhs.minor_);
-}
-
-inline compute_capability_t make_compute_capability(unsigned combined) noexcept
-{
-	return compute_capability_t::from_combined_number(combined);
-}
-
-inline compute_capability_t make_compute_capability(unsigned major, unsigned minor) noexcept
-{
-	return { major, minor };
-}
+inline constexpr compute_capability_t make_compute_capability(unsigned combined) noexcept;
+inline constexpr compute_capability_t make_compute_capability(unsigned major, unsigned minor) noexcept;
 
 /**
  * @brief A structure holding a collection various properties of a device
@@ -190,11 +121,8 @@ struct properties_t : public cudaDeviceProp {
 	properties_t() = default;
 	properties_t(const cudaDeviceProp& cdp) noexcept : cudaDeviceProp(cdp) { };
 	properties_t(cudaDeviceProp&& cdp) noexcept : cudaDeviceProp(cdp) { };
-	bool usable_for_compute() const noexcept
-	{
-		return computeMode != cudaComputeModeProhibited;
-	}
-	compute_capability_t compute_capability() const { return { (unsigned) major, (unsigned) minor }; }
+	bool usable_for_compute() const noexcept;
+	compute_capability_t compute_capability() const noexcept { return { (unsigned) major, (unsigned) minor }; }
 	compute_architecture_t compute_architecture() const noexcept { return { (unsigned) major }; };
 	pci_location_t pci_id() const noexcept { return { pciDomainID, pciBusID, pciDeviceID }; }
 
@@ -212,5 +140,7 @@ struct properties_t : public cudaDeviceProp {
 
 } // namespace device
 } // namespace cuda
+
+#include "detail/device_properties.hpp"
 
 #endif // CUDA_API_WRAPPERS_DEVICE_PROPERTIES_HPP_
