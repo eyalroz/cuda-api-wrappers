aa36f9cabef9fd35cf3de2b85e4408c99154fc13
Eyal Rozenberg <eyalroz@technion.ac.il>
2020-07-12 22:06:26 +0300

    Fixes #205, regarding #147:
    * De-templatized the `cuda::device::current::scoped_setter_t` class; there's no longer an option to assume the device is already the current one.
    * De-templatized the corresponding `cuda::device::current::detail::scoped_setter_t` class.
    * Not using the redundant `device::` namespace in some places in the `detail::scoped_setter_t` class
    * Changed the capitalization on the scoped setter class alias within stream_t

--- src/cuda/api/memory.hpp
+++ src/cuda/api/memory.hpp
@@ -177,7 +177,7 @@ inline region_t allocate(size_t num_bytes)
 
 inline region_t allocate(cuda::device::id_t device_id, size_t size_in_bytes)
 {
-	cuda::device::current::detail::scoped_override_t<> set_device_for_this_scope(device_id);
+	cuda::device::current::detail::scoped_override_t set_device_for_this_scope(device_id);
 	return memory::device::detail::allocate(size_in_bytes);
 }
 
@@ -1117,7 +1117,7 @@ inline region_t allocate(
 	size_t                num_bytes,
 	initial_visibility_t  initial_visibility = initial_visibility_t::to_all_devices)
 {
-	cuda::device::current::detail::scoped_override_t<> set_device_for_this_scope(device_id);
+	cuda::device::current::detail::scoped_override_t set_device_for_this_scope(device_id);
 	return detail::allocate(num_bytes, initial_visibility);
 }
 
@@ -1298,7 +1298,7 @@ inline region_pair allocate(
 	size_t              size_in_bytes,
 	allocation_options  options)
 {
-	cuda::device::current::detail::scoped_override_t<> set_device_for_this_scope(device_id);
+	cuda::device::current::detail::scoped_override_t set_device_for_this_scope(device_id);
 	return detail::allocate(size_in_bytes, options);
 }
 
