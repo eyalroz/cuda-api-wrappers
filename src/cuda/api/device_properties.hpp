9c087322caba8de9f2b05e3e7e435c06df02678a
Eyal Rozenberg <eyalroz@technion.ac.il>
2020-07-17 22:38:02 +0300

    Fixes #209: Avoiding clang++ warnings about missing braces
    * Added braces around the initializer of `architecture_t` when aggregate-initializing a `computer_capability_t`

--- src/cuda/api/device_properties.hpp
+++ src/cuda/api/device_properties.hpp
@@ -130,7 +130,7 @@ struct properties_t : public cudaDeviceProp {
 	properties_t(const cudaDeviceProp& cdp) noexcept : cudaDeviceProp(cdp) { };
 	properties_t(cudaDeviceProp&& cdp) noexcept : cudaDeviceProp(cdp) { };
 	bool usable_for_compute() const noexcept;
-	compute_capability_t compute_capability() const noexcept { return { (unsigned) major, (unsigned) minor }; }
+	compute_capability_t compute_capability() const noexcept { return { { (unsigned) major }, (unsigned) minor }; }
 	compute_architecture_t compute_architecture() const noexcept { return { (unsigned) major }; };
 	pci_location_t pci_id() const noexcept { return { pciDomainID, pciBusID, pciDeviceID }; }
 
