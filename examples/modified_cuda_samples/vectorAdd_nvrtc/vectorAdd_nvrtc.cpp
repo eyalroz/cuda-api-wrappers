c1873a6de1011b52f7fbe4281fbd6967a93b4375
Eyal Rozenberg <eyalroz@technion.ac.il>
2020-10-07 01:25:41 +0300

    Fixes #228, #262: Added NVRTC support! Including support for the compilation options added to NVRTC in CUDA 11.4/11.5, and most other options, excluding the diagnostic error/suppress/ignore pair. Also regards #128 - this essentially adds much of what NVIDIA jitify provides.

--- examples/modified_cuda_samples/vectorAdd_nvrtc/vectorAdd_nvrtc.cpp
+++ examples/modified_cuda_samples/vectorAdd_nvrtc/vectorAdd_nvrtc.cpp
@@ -1,22 +1,30 @@
 /**
- * Derived from the nVIDIA CUDA 8.0 samples by
+ * Derived from the nVIDIA CUDA 10.0 samples by
  *
- *   Eyal Rozenberg
+ *   Eyal Rozenberg <eyalroz@technion.ac.il>
  *
  * The derivation is specifically permitted in the nVIDIA CUDA Samples EULA
  * and the deriver is the owner of this code according to the EULA.
  *
  * Use this reasonably. If you want to discuss licensing formalities, please
  * contact the author.
+ *
+ * This version differs from the other vectorAdd example in that managed memory is
+ * used instead of regular host and device memory.
  */
 
 #include "../../common.hpp"
 
-#include <cuda/api.hpp>
-
+#include <cuda/nvrtc.hpp>
 #include <iostream>
-#include <memory>
-#include <algorithm>
+#include <cmath>
+
+const char* vectorAdd_source = R"(
+
+/**
+ * Computes the vector addition of A and B into C. The 3 vectors have the same
+ * number of elements numElements.
+ */
 
 __global__ void vectorAdd(const float *A, const float *B, float *C, int numElements)
 {
@@ -24,27 +32,35 @@ __global__ void vectorAdd(const float *A, const float *B, float *C, int numEleme
 	if (i < numElements) { C[i] = A[i] + B[i]; }
 }
 
-int main()
-{
-	if (cuda::device::count() == 0) {
-		std::cerr << "No CUDA devices on this system" << "\n";
-		exit(EXIT_FAILURE);
-	}
+)";
 
+int main(void)
+{
 	int numElements = 50000;
 	size_t size = numElements * sizeof(float);
+	auto kernel_name = "vectorAdd";
+
 	std::cout << "[Vector addition of " << numElements << " elements]\n";
 
+	auto device = cuda::device::current::get();
+	auto program = cuda::rtc::program::create(kernel_name, vectorAdd_source);
+	program.register_global(kernel_name);
+	program.compile_for(device);
+	auto mangled_kernel_name = program.get_mangling_of(kernel_name);
+
+	auto context = cuda::device::current::get().primary_context();
+	auto module = cuda::module::create(context, program);
+	auto vectorAdd = module.get_kernel(mangled_kernel_name);
+
 	// If we could rely on C++14, we would  use std::make_unique
-	auto h_A = std::unique_ptr<float>(new float[numElements]);
-	auto h_B = std::unique_ptr<float>(new float[numElements]);
-	auto h_C = std::unique_ptr<float>(new float[numElements]);
+	auto h_A = std::unique_ptr<float[]>(new float[numElements]);
+	auto h_B = std::unique_ptr<float[]>(new float[numElements]);
+	auto h_C = std::unique_ptr<float[]>(new float[numElements]);
 
 	auto generator = []() { return rand() / (float) RAND_MAX; };
 	std::generate(h_A.get(), h_A.get() + numElements, generator);
 	std::generate(h_B.get(), h_B.get() + numElements, generator);
 
-	auto device = cuda::device::current::get();
 	auto d_A = cuda::memory::device::make_unique<float[]>(device, numElements);
 	auto d_B = cuda::memory::device::make_unique<float[]>(device, numElements);
 	auto d_C = cuda::memory::device::make_unique<float[]>(device, numElements);
@@ -61,7 +77,7 @@ int main()
 
 	cuda::launch(
 		vectorAdd,
-		cuda::make_launch_config( blocksPerGrid, threadsPerBlock ),
+		cuda::launch_configuration_t( blocksPerGrid, threadsPerBlock ),
 		d_A.get(), d_B.get(), d_C.get(), numElements
 	);
 
@@ -69,13 +85,14 @@ int main()
 
 	// Verify that the result vector is correct
 	for (int i = 0; i < numElements; ++i) {
-		if (fabs(h_A.get()[i] + h_B.get()[i] - h_C.get()[i]) > 1e-5)  {
+		if (std::fabs(h_A.get()[i] + h_B.get()[i] - h_C.get()[i]) > 1e-5)  {
 			std::cerr << "Result verification failed at element " << i << "\n";
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	std::cout << "Test PASSED\n";
-	std::cout << "SUCCESS\n";
+    std::cout << "Test PASSED\n";
+    std::cout << "SUCCESS\n";
 }
+
 
---
+++ src/cuda/nvrtc/versions.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file nvrtc/versions.hpp
+ *
+ * @brief Complementing file for `cuda/api/versions.hpp` for obtaining
+ * a version number for the NVRTC library
+ */
+#pragma once
+#ifndef CUDA_API_WRAPPERS_NVRTC_VERSIONS_HPP_
+#define CUDA_API_WRAPPERS_NVRTC_VERSIONS_HPP_
+
+#include <cuda/nvrtc/error.hpp>
+#include <cuda/api/versions.hpp>
+
+namespace cuda {
+
+namespace version_numbers {
+
+/**
+ * Obtain the NVRTC library version
+ */
+inline version_t nvrtc() {
+	version_t version;
+	auto status = nvrtcVersion(&version.major, &version.minor);
+	throw_if_error(status, "Failed obtaining the NVRTC library version");
+	return version;
+}
+
+} // namespace version_numbers
+} // namespace cuda
+
+#endif // CUDA_API_WRAPPERS_NVRTC_VERSIONS_HPP_
