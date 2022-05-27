49f5beda7ffe5daf17c15f4ffe10849c284b0ddc
Eyal Rozenberg <eyalroz1@gmx.com>
2022-05-27 21:02:00 +0300

    Fixes #337: C++ standard version check fix.
    * Now using preprocessor checks for the C++ standard version which also take into account MSVC peculiarities - like was already the case in `types.hpp`.
    * Unified the error message on too-early C++ standard version.

--- src/cuda/nvtx.hpp
+++ src/cuda/nvtx.hpp
@@ -8,7 +8,9 @@
 #ifndef CUDA_NVTX_WRAPPERS_HPP_
 #define CUDA_NVTX_WRAPPERS_HPP_
 
-static_assert(__cplusplus >= 201103L, "The CUDA NVTX API wrappers can only be compiled with C++11 or a later version of the C++ language standard");
+#if (__cplusplus < 201103L && (!defined(_MSVC_LANG) || _MSVC_LANG < 201103L))
+#error "The CUDA API headers can only be compiled with C++11 or a later version of the C++ language standard"
+#endif
 
 #include <cuda/nvtx/profiling.hpp>
 
