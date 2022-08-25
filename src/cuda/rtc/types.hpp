1c5dfb65199dd0bbb446b610e6cbe7b096ad4607
Eyal Rozenberg <eyalroz1@gmx.com>
2022-08-25 17:18:27 +0300

    Fixes #385: Now supporting the PTX compilation mechanism.
    * Now using a `source_kind_t` enum with one value for PTX and another for CUDA C++
    * Added some error-reporting machinery using `source_kind_t` for code utilizing NVRTC and the PTX compiler (eac of which having their own error codes)
    * Templated RTC-related class: `compilation_options_t`, `program_t`, `compilation_output_t`
    * Added a dependency on the `nvptxcompiler` library (with allowance for when only the static version is available)
    * Added another example program - another variant of `vectorAdd`, very similar to vectorAdd_nvrtc, except that it uses a PTX source and PTX compilation to get an executable kernel

--- src/cuda/rtc/types.hpp
+++ src/cuda/rtc/types.hpp
@@ -10,15 +10,14 @@
 #include <cuda/api/types.hpp>
 
 #include <nvrtc.h>
+#include <nvPTXCompiler.h>
 
 #include <vector>
 
 #if __cplusplus >= 201703L
-// #include <filesystem>
 #include <string_view>
 namespace cuda {
 using string_view = ::std::string_view;
-// namespace filesystem = ::std::filesystem;
 }
 #else
 #include <cuda/rtc/detail/string_view.hpp>
@@ -28,6 +27,11 @@ using string_view = bpstd::string_view;
 #endif
 
 namespace cuda {
+
+enum source_kind_t {
+	cuda_cpp = 0,
+	ptx = 1
+};
 
 // The C++ standard library doesn't offer ::std::dynarray (although it almost did),
 // and we won't introduce our own here. So...
@@ -44,11 +48,72 @@ using dynarray = ::std::vector<T>;
  */
 namespace rtc {
 
+using const_cstrings_span = span<const char* const>;
+using const_cstring_pairs_span = span<::std::pair<const char* const, const char* const>>;
+
 namespace program {
 
-using handle_t = nvrtcProgram;
+namespace detail_ {
+
+constexpr char const *kind_name(source_kind_t kind)
+{
+	return (kind == cuda_cpp) ? "CUDA C++" : "PTX";
+}
+
+} // namespace detail_
 
 } // namespace program
+
+namespace detail_ {
+
+template <source_kind_t Kind> struct types {};
+
+template <> struct types<cuda_cpp> {
+	using handle_type = nvrtcProgram;
+	using status_type = nvrtcResult;
+	enum named_status : std::underlying_type<status_type>::type {
+		success = NVRTC_SUCCESS,
+		out_of_memory = NVRTC_ERROR_OUT_OF_MEMORY,
+		program_creation_failure = NVRTC_ERROR_OUT_OF_MEMORY,
+		invalid_input = NVRTC_ERROR_PROGRAM_CREATION_FAILURE,
+		invalid_program = NVRTC_ERROR_INVALID_PROGRAM,
+		invalid_option = NVRTC_ERROR_INVALID_OPTION,
+		compilation_failure = NVRTC_ERROR_COMPILATION,
+		builtin_operation_failure = NVRTC_ERROR_BUILTIN_OPERATION_FAILURE,
+		no_registered_globals_after_compilation = NVRTC_ERROR_NO_NAME_EXPRESSIONS_AFTER_COMPILATION,
+		no_lowered_names_before_compilation = NVRTC_ERROR_NO_LOWERED_NAMES_BEFORE_COMPILATION,
+		invalid_expression_to_register_as_global = NVRTC_ERROR_NAME_EXPRESSION_NOT_VALID,
+		internal_error = NVRTC_ERROR_INTERNAL_ERROR,
+	};
+};
+
+template <> struct types<ptx> {
+	using handle_type = nvPTXCompilerHandle;
+	using status_type = nvPTXCompileResult;
+	enum named_status : std::underlying_type<status_type>::type {
+		success = NVPTXCOMPILE_SUCCESS,
+		invalid_handle = NVPTXCOMPILE_ERROR_INVALID_COMPILER_HANDLE,
+		invalid_program_handle = NVPTXCOMPILE_ERROR_INVALID_COMPILER_HANDLE,
+		out_of_memory = NVPTXCOMPILE_ERROR_OUT_OF_MEMORY,
+		invalid_input = NVPTXCOMPILE_ERROR_INVALID_INPUT,
+		compilation_invocation_incomplete = NVPTXCOMPILE_ERROR_COMPILER_INVOCATION_INCOMPLETE,
+		compilation_failure = NVPTXCOMPILE_ERROR_COMPILATION_FAILURE,
+		unsupported_ptx_version = NVPTXCOMPILE_ERROR_UNSUPPORTED_PTX_VERSION,
+		internal_error = NVPTXCOMPILE_ERROR_INTERNAL,
+	};
+};
+
+} // namespace detail_
+
+namespace program {
+
+template <source_kind_t Kind>
+using handle_t = typename cuda::rtc::detail_::types<Kind>::handle_type;
+
+} // namespace program
+
+template <source_kind_t Kind>
+using status_t = typename detail_::types<Kind>::status_type;
 
 } // namespace rtc
 
