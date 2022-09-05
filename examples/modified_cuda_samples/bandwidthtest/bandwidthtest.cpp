9c11950d919e14a2f683ccdbde8040809e6b2ca4
Eyal Rozenberg <eyalroz1@gmx.com>
2022-09-05 12:39:39 +0300

    Regards #398: Added the bandwidthtest sample, but without adapting it to the API wrappers
    Changes from the original:
    
    * Altered the license/copyright notice block to introduce myself into it
    * Fixed compiler warnings:
      * Same type for loop variable and sentinel value
      * `size_t` for sizes
      * `char const*` rather than `char*` for the buffer pointer
      * Extended const to another variable
    
    Still in C-land though.

---
+++ examples/modified_cuda_samples/bandwidthtest/bandwidthtest.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2022, Eyal Rozenberg, under the terms of the 3-clause
+ * BSD software license; see the LICENSE file accompanying this
+ * repository.
+ *
+ * Copyright notice and license for the original code:
+ * Copyright (c) 1993-2015, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cuda_runtime.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+// Convenience function for checking CUDA runtime API results
+// can be wrapped around any runtime API call. No-op in release builds.
+inline
+cudaError_t checkCuda(cudaError_t result)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+	if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+#endif
+	return result;
+}
+
+void profileCopies(float        *h_a,
+				   float        *h_b,
+				   float        *d,
+				   size_t       nElements,
+				   char const   *desc)
+{
+	printf("\n%s transfers\n", desc);
+
+	size_t bytes = nElements * sizeof(float);
+
+	// events for timing
+	cudaEvent_t startEvent, stopEvent;
+
+	checkCuda( cudaEventCreate(&startEvent) );
+	checkCuda( cudaEventCreate(&stopEvent) );
+
+	checkCuda( cudaEventRecord(startEvent, 0) );
+	checkCuda( cudaMemcpy(d, h_a, bytes, cudaMemcpyHostToDevice) );
+	checkCuda( cudaEventRecord(stopEvent, 0) );
+	checkCuda( cudaEventSynchronize(stopEvent) );
+
+	float time;
+	checkCuda( cudaEventElapsedTime(&time, startEvent, stopEvent) );
+	printf("  Host to Device bandwidth (GB/s): %f\n", bytes * 1e-6 / time);
+
+	checkCuda( cudaEventRecord(startEvent, 0) );
+	checkCuda( cudaMemcpy(h_b, d, bytes, cudaMemcpyDeviceToHost) );
+	checkCuda( cudaEventRecord(stopEvent, 0) );
+	checkCuda( cudaEventSynchronize(stopEvent) );
+
+	checkCuda( cudaEventElapsedTime(&time, startEvent, stopEvent) );
+	printf("  Device to Host bandwidth (GB/s): %f\n", bytes * 1e-6 / time);
+
+	for (size_t i = 0; i < nElements; ++i) {
+		if (h_a[i] != h_b[i]) {
+			printf("*** %s transfers failed ***", desc);
+			break;
+		}
+	}
+
+	// clean up events
+	checkCuda( cudaEventDestroy(startEvent) );
+	checkCuda( cudaEventDestroy(stopEvent) );
+}
+
+int main()
+{
+	const size_t nElements = 4*1024*1024;
+	const size_t bytes = nElements * sizeof(float);
+
+	// host arrays
+	float *h_aPageable, *h_bPageable;
+	float *h_aPinned, *h_bPinned;
+
+	// device array
+	float *d_a;
+
+	// allocate and initialize
+	h_aPageable = (float*)malloc(bytes);                    // host pageable
+	h_bPageable = (float*)malloc(bytes);                    // host pageable
+	checkCuda( cudaMallocHost((void**)&h_aPinned, bytes) ); // host pinned
+	checkCuda( cudaMallocHost((void**)&h_bPinned, bytes) ); // host pinned
+	checkCuda( cudaMalloc((void**)&d_a, bytes) );           // device
+
+	for (size_t i = 0; i < nElements; ++i) h_aPageable[i] = i;
+	memcpy(h_aPinned, h_aPageable, bytes);
+	memset(h_bPageable, 0, bytes);
+	memset(h_bPinned, 0, bytes);
+
+	// output device info and transfer size
+	cudaDeviceProp prop;
+	checkCuda( cudaGetDeviceProperties(&prop, 0) );
+
+	printf("\nDevice: %s\n", prop.name);
+	printf("Transfer size (MB): %zu\n", bytes / (size_t) (1024 * 1024));
+
+	// perform copies and report bandwidth
+	profileCopies(h_aPageable, h_bPageable, d_a, nElements, "Pageable");
+	profileCopies(h_aPinned, h_bPinned, d_a, nElements, "Pinned");
+
+	printf("\n");
+
+	// cleanup
+	cudaFree(d_a);
+	cudaFreeHost(h_aPinned);
+	cudaFreeHost(h_bPinned);
+	free(h_aPageable);
+	free(h_bPageable);
+
+	return 0;
+}
