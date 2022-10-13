06c32ed48837209f7e6678dbe94f74562b0d5fa6
Eyal Rozenberg <eyal.rozenberg@ge.com>
2022-10-13 17:33:24 +0300

    Added an example program which compiles using C++14 (unlike all the rest, which use C++11).

---
+++ examples/other/cpp_14/main.cpp
@@ -0,0 +1,28 @@
+#include <cuda/api.hpp>
+#include <cuda/nvtx.hpp>
+#include <cuda/nvrtc.hpp>
+
+#include <cstdlib>
+#include <iostream>
+
+cuda::device::id_t get_current_device_id()
+{
+    auto device = cuda::device::current::get();
+    return device.id();
+}
+
+int main() 
+{
+	auto count = cuda::device::count();
+
+	if (count > 0) { 
+		get_current_device_id();
+	}
+
+	auto nvrtc_version = cuda::version_numbers::nvrtc();
+	(void) nvrtc_version;
+
+	auto nvtx_color_yellow = cuda::profiling::color_t::from_hex(0x0FFFF00);
+	(void) nvtx_color_yellow;
+	std::cout << "SUCCESS\n";
+}
