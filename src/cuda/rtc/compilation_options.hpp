f280480e8d2523913d4b6d6e9bfd252dc2c115bd
Eyal Rozenberg <eyalroz1@gmx.com>
2023-02-05 15:50:38 +0200

    Removed an unused  utility function

--- src/cuda/rtc/compilation_options.hpp
+++ src/cuda/rtc/compilation_options.hpp
@@ -24,22 +24,6 @@
 
 namespace cuda {
 
-namespace detail_ {
-
-template <class F>
-void for_each_argument(F) { }
-
-template <class F, class... Args>
-void for_each_argument(F f, Args&&... args) {
-	using arrT = int[];
-	static_cast<void>(arrT{(f(::std::forward<Args>(args)), 0)...});
-// This:
-//	[](...){}((f(::std::forward<Args>(args)), 0)...);
-// doesn't guarantee execution order
-}
-
-} // namespaced detail_
-
 namespace rtc {
 
 enum class cpp_dialect_t {
