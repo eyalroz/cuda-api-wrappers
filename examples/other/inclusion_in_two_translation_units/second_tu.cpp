1b4cc3549fce40477b3f8fd15f73f64ced9b9e1d
Eyal Rozenberg <eyalroz1@gmx.com>
2023-04-24 01:04:57 +0300

    Regards #505, #506: Not building profiling-related examples on Windows with CUDA versions before 12, due to trouble locating NVTX headers

--- examples/other/inclusion_in_two_translation_units/second_tu.cpp
+++ examples/other/inclusion_in_two_translation_units/second_tu.cpp
@@ -1,5 +1,8 @@
 #include <cuda/api.hpp>
+#if !defined(_MSC_VER) || CUDA_VERSION >= 12000
+// MSVC + CMake on Windows has trouble with NVTX header location
 #include <cuda/nvtx.hpp>
+#endif
 #include <cuda/rtc.hpp>
 
 cuda::device::id_t get_current_device_id()
