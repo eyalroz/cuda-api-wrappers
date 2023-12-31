0246310056ed6e50f8ce014bdaef840e074098f8
Eyal Rozenberg <eyal.rozenberg@ge.com>
2023-12-31 16:51:00 +0200

    Fixes #563: Now enabling the C++ language for the project, which is necessary for relying on the Threads library

--- cmake/cuda-api-wrappers-config.cmake
+++ cmake/cuda-api-wrappers-config.cmake
@@ -1,7 +1,8 @@
 include(CMakeFindDependencyMacro)
 
+enable_language(CXX)
 set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 find_dependency(Threads)
 find_dependency(CUDAToolkit)
 
-include("${CURRENT_LIST_DIR}/cuda-api-wrappers-targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/cuda-api-wrappers-targets.cmake")
