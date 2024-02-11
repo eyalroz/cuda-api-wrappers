ccf758d268d9a91048b9b8afe1cbcca74681e74a
Eyal Rozenberg <eyalroz1@gmx.com>
2024-02-11 23:26:47 +0200

    Regards #291: Added support for untyped, owned, memory regions:
    * Added a `unique_region` class - non-templated
    * `unique_region`'s "decay" into `cuda::memory::region_t`'s - so there's no need to reimplement half the world for them
    * Added a missing variant of `cuda::memory::copy()`
    * Added an example program based on vectorAdd, which uses unique_regions (I would have replaced vectorAdd with it - had it not requirted to lines of source more than before...)
    * `region_t`'s now have an `as_span` method, as well as an `operator span()`
    * Removed some commented-out unique-pointer-related code

---
+++ src/cuda/api/multi_wrapper_impls/unique_region.hpp
@@ -0,0 +1,122 @@
+/**
+ * @file
+ *
+ * @brief Implementations of @ref `make_unique_region()` functions
+ */
+#pragma once
+#ifndef MULTI_WRAPPER_IMPLS_UNIQUE_REGION_HPP_
+#define MULTI_WRAPPER_IMPLS_UNIQUE_REGION_HPP_
+
+#include "../unique_region.hpp"
+#include "../types.hpp"
+
+namespace cuda {
+
+namespace memory {
+
+namespace device {
+
+inline unique_region make_unique_region(const context_t& context, cuda::size_t num_elements)
+{
+	return detail_::make_unique_region(context.handle(), num_elements);
+}
+
+/**
+ * @brief Create a variant of ::std::unique_pointer for an array in
+ * device-global memory
+ *
+ * @tparam T  an array type; _not_ the type of individual elements
+ *
+ * @param device        on which to construct the array of elements
+ * @param num_elements  the number of elements to allocate
+ * @return an ::std::unique_ptr pointing to the constructed T array
+ */
+inline unique_region make_unique_region(const device_t& device, size_t num_elements)
+{
+	auto pc = device.primary_context();
+	return make_unique_region(pc, num_elements);
+}
+
+/**
+ * @brief Create a variant of ::std::unique_pointer for an array in
+ * device-global memory on the current device.
+ *
+ * @note The allocation will be made in the device's primary context -
+ * which will be created if it has not yet been.
+ *
+ * @tparam T  an array type; _not_ the type of individual elements
+ *
+ * @param num_elements  the number of elements to allocate
+ *
+ * @return an ::std::unique_ptr pointing to the constructed T array
+ */
+inline unique_region make_unique_region(size_t num_elements)
+{
+	auto current_device_id = cuda::device::current::detail_::get_id();
+	auto pc = cuda::device::primary_context::detail_::leaky_get(current_device_id);
+	return make_unique_region(pc, num_elements);
+}
+
+} // namespace device
+
+namespace host {
+
+inline unique_region make_unique_region(
+	const context_t&    context,
+	size_t              num_bytes,
+	allocation_options  options)
+{
+	CAW_SET_SCOPE_CONTEXT(context.handle());
+	return unique_region{ allocate(num_bytes, options) };
+}
+
+inline unique_region make_unique_region(const device_t& device, size_t num_bytes)
+{
+	auto pc = device.primary_context();
+	return make_unique_region(pc, num_bytes);
+}
+
+inline unique_region make_unique_region(size_t num_bytes)
+{
+	return unique_region { allocate(num_bytes) };
+}
+
+} // namespace host
+
+namespace managed {
+
+inline unique_region make_unique_region(
+	const context_t&      context,
+	size_t                num_bytes,
+	initial_visibility_t  initial_visibility)
+{
+	CAW_SET_SCOPE_CONTEXT(context.handle());
+	return unique_region { detail_::allocate_in_current_context(num_bytes, initial_visibility) };
+}
+
+inline unique_region make_unique_region(
+	const device_t&       device,
+	size_t                num_bytes,
+	initial_visibility_t  initial_visibility)
+{
+	auto pc = device.primary_context();
+	return make_unique_region(pc, num_bytes, initial_visibility);
+}
+
+inline unique_region make_unique_region(
+	size_t                num_bytes,
+	initial_visibility_t  initial_visibility)
+{
+	auto current_device_id = cuda::device::current::detail_::get_id();
+	auto pc = cuda::device::primary_context::detail_::leaky_get(current_device_id);
+	return make_unique_region(pc, num_bytes, initial_visibility);
+}
+
+} // namespace managed
+
+} // namespace memory
+
+} // namespace cuda
+
+#endif // MULTI_WRAPPER_IMPLS_UNIQUE_REGION_HPP_
+
