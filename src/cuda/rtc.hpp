6000f64510c343651e639d09e1c3d6babc19f268
Eyal Rozenberg <eyalroz1@gmx.com>
2024-03-01 20:51:45 +0200

    Removed redundant preprocessor guards for the minimum C++ language version; everything goes through `types.hpp` which has that guard

--- src/cuda/rtc.hpp
+++ src/cuda/rtc.hpp
@@ -9,10 +9,6 @@
 #ifndef CUDA_NVRTC_WRAPPERS_HPP_
 #define CUDA_NVRTC_WRAPPERS_HPP_
 
-#if (__cplusplus < 201103L && (!defined(_MSVC_LANG) || _MSVC_LANG < 201103L))
-#error "The CUDA API headers can only be compiled with C++11 or a later version of the C++ language standard"
-#endif
-
 #include "rtc/error.hpp"
 #include "rtc/compilation_options.hpp"
 #include "rtc/versions.hpp"
