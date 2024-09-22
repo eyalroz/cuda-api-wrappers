47a18d399958a5515074734ca17afaf443ad43c4
Eyal Rozenberg <eyalroz1@gmx.com>
2024-09-22 09:49:17 +0300

    GitHub actions: Added runners for building with CUDA 12.5.1 and 12.6.0 (on Linux, building those both with ubuntu 20.04 and 24.04)

--- .github/action-scripts/install-cuda-windows.ps1
+++ .github/action-scripts/install-cuda-windows.ps1
@@ -51,6 +51,8 @@ $CUDA_KNOWN_URLS = @{
     "12.3.0" = "https://developer.download.nvidia.com/compute/cuda/12.3.0/network_installers/cuda_12.3.0_windows_network.exe";
     "12.3.1" = "https://developer.download.nvidia.com/compute/cuda/12.3.1/network_installers/cuda_12.3.1_windows_network.exe";
     "12.4.1" = "https://developer.download.nvidia.com/compute/cuda/12.4.1/network_installers/cuda_12.4.1_windows_network.exe";
+    "12.5.1" = "https://developer.download.nvidia.com/compute/cuda/12.5.1/network_installers/cuda_12.5.1_windows_network.exe";
+    "12.6.0" = "https://developer.download.nvidia.com/compute/cuda/12.6.0/network_installers/cuda_12.6.0_windows_network.exe";
 }
 
 # @todo - change this to be based on _MSC_VER intead, or invert it to be CUDA keyed instead?
