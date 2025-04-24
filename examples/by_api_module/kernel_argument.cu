d97f009be5538c86e2ad02d9281953a7ca3c0f95
Grzegorz Głowacki <gglowacki@autonomous-systems.pl>
2025-04-24 12:38:59 +0200

    usage example

---
+++ examples/by_api_module/kernel_argument.cu
@@ -0,0 +1,103 @@
+/**
+ * An example program utilizing most/all calls from the CUDA
+ * Runtime API module:
+ *
+ *   Kernel argument passing
+ *
+ */
+#include "../common.hpp"
+#include "cuda/api/traits.hpp"
+
+using std::printf;
+
+class NonTriviallyCopyable {
+public:
+  NonTriviallyCopyable() = default;
+  NonTriviallyCopyable(const char *name) {
+    printf("NonTriviallyCopyable constructor called\n");
+    std::strncpy(name_, name, sizeof(name_));
+    name_[sizeof(name_) - 1] = '\0';
+  }
+
+  NonTriviallyCopyable(NonTriviallyCopyable &&) = default;
+
+  NonTriviallyCopyable(const NonTriviallyCopyable &other) {
+    printf("NonTriviallyCopyable copy constructor called\n");
+    std::strncpy(name_, other.name_, sizeof(name_));
+    name_[sizeof(name_) - 1] = '\0';
+  }
+
+  ~NonTriviallyCopyable() {
+    printf("NonTriviallyCopyable destructor called\n");
+  }
+
+  __host__ __device__ NonTriviallyCopyable &
+  operator=(const NonTriviallyCopyable &) = default;
+  NonTriviallyCopyable &operator=(NonTriviallyCopyable &&) = default;
+  // Non-trivial destructor
+
+  __host__ __device__ const char *name() const noexcept { return name_; }
+
+private:
+  char name_[10];
+};
+static_assert(!std::is_trivially_copyable<NonTriviallyCopyable>::value,
+              "NonTriviallyCopyable should not be trivially copyable");
+
+template <>
+struct cuda::traits::is_valid_kernel_argument<NonTriviallyCopyable>
+    : ::std::true_type {};
+
+__global__ void print_message(NonTriviallyCopyable message) {
+  if (threadIdx.x == 0 && blockIdx.x == 0) {
+    printf("Kernel says: %s\n", (const char *)message.name());
+  }
+}
+
+__global__ void copy_me(NonTriviallyCopyable in, NonTriviallyCopyable *out) {
+  if (threadIdx.x == 0 && blockIdx.x == 0) {
+    *out = in;
+    printf("Kernel copied me: %s\n", (const char *)out->name());
+  }
+}
+
+int main(int argc, char **argv) {
+
+  cuda::launch_configuration_t single_thread_config{1, 1};
+
+  // Being very cavalier about our command-line arguments here...
+  cuda::device::id_t device_id =
+      (argc > 1) ? std::stoi(argv[1]) : cuda::device::default_device_id;
+
+  if (cuda::device::count() == 0) {
+    die_("No CUDA devices on this system");
+  }
+
+  auto device = cuda::device::get(device_id).make_current();
+
+  std::cout << "Using CUDA device " << device.name() << " (having device ID "
+            << device.id() << ")\n";
+
+  {
+    auto stream =
+        cuda::device::current::get().create_stream(cuda::stream::sync);
+    std::cout << "A new CUDA stream with no priority specified defaults to "
+                 "having priority "
+              << stream.priority() << ".\n";
+    stream.enqueue.kernel_launch(print_message, single_thread_config,
+                                 NonTriviallyCopyable("It works!"));
+
+	auto dest_mem = cuda::memory::device::make_unique_region(device, sizeof(NonTriviallyCopyable));
+    auto dest_span = dest_mem.as_span<NonTriviallyCopyable>();
+
+    NonTriviallyCopyable src("Source");
+    NonTriviallyCopyable dest;
+    stream.enqueue.kernel_launch(copy_me, single_thread_config, src, dest_span.data());
+	stream.enqueue.copy(&dest, dest_span);
+    stream.synchronize();
+    assert(strcmp(dest.name(), src.name()) == 0);
+    printf("Host says: %s\n", (const char *)dest.name());
+  }
+
+  return EXIT_SUCCESS;
+}
